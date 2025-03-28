30f22eb4bf47680bc6e8d0840b8379dfb7c80844
ysh <yan9hwa@gmail.com>
2025-03-28 11:48:05 +0900

    test

--- zero/app/auth/SessionProvider.tsx
+++
@@ -1,7 +0,0 @@
-"use client"; // ✅ 클라이언트 컴포넌트 선언
-
-import { SessionProvider } from "next-auth/react";
-
-export default function AuthProvider({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
