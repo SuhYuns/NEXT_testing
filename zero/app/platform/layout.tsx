c10842b1ced2148db880c60f72515c886234f90d
ysh <yan9hwa@gmail.com>
2025-04-24 14:59:38 +0900

    logo update

--- zero/app/platform/layout.tsx
+++ zero/app/platform/layout.tsx
@@ -32,13 +32,17 @@ export default function RootLayout({
           <a href="/platform">
             <div className="
                 h-[300px]
+                w-auto
                 relative
-                bg-[url(https://mkgpxawmsyiucaitvdgf.supabase.co/storage/v1/object/public/blog-uploads//zerobar.jpg)] 
-                mb-10
-                mx-auto
-            ">
 
-                <div className="absolute inset-0 bg-black opacity-50 z-0 align-center"></div>
+                bg-[url('https://mkgpxawmsyiucaitvdgf.supabase.co/storage/v1/object/public/blog-uploads/zerobar.jpg')]
+                bg-center    /* 수평·수직 중앙 정렬 */
+                bg-cover     /* 비율 유지하며 가득 채우기 */
+                bg-no-repeat /* 반복 방지 *
+            ">
+              
+
+                <div className="absolute inset-0 bg-black opacity-40 z-0 align-center"></div>
 
                 {/* 텍스트 컨텐츠 - z-index를 오버레이보다 높게 설정 */}
                   {/* <div className="relative z-10 flex items-center justify-center h-full">
@@ -46,6 +50,10 @@ export default function RootLayout({
                       <h1 className="text-5xl font-bold text-white w-f">NEXT THINKING</h1>
                   </div> */}
                 
+            </div>
+            <div className="bg-gray-800 opacity-40 py-10
+                mb-5">
+
             </div>
             </a>
 
