510fa149c9d385723077c90a75a23b49041b883c
yan9hwa <104848737+SuhYuns@users.noreply.github.com>
2025-05-29 17:55:10 +0900

    update

--- zero/middleware.ts
+++ zero/middleware.ts
@@ -1,16 +1,19 @@
+// zero/middleware.ts
 import { NextRequest, NextResponse } from 'next/server';
 
 export const config = { matcher: ['/'] };
 
 export function middleware(req: NextRequest) {
-  const host = req.headers.get('host') ?? 'no-host';
-  console.log('[middleware] host:', host);
+  const host = req.headers.get('host') || '';
 
   if (host.endsWith('nextzerobar.org')) {
-    return NextResponse.rewrite(new URL('/app/platform', req.url));
+    // ‘app’ 폴더 제거
+    return NextResponse.rewrite(new URL('/platform' + req.nextUrl.search, req.url));
   }
+
   if (host === 'nextgroup.vercel.app') {
-    return NextResponse.rewrite(new URL('/app', req.url));
+    return NextResponse.rewrite(new URL('/int' + req.nextUrl.search, req.url));
   }
+
   return NextResponse.next();
 }
