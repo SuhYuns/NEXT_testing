5262981a80323b049a9250bb2a51d9053283ff85
yan9hwa <104848737+SuhYuns@users.noreply.github.com>
2025-06-10 08:04:32 +0900

    update

---
+++ zero/app/int/info/desk/manage/page.tsx
@@ -0,0 +1,13 @@
+'use client'; // Next.js 13 이상에서 클라이언트 컴포넌트로 명시해야 함
+
+import { useEffect } from 'react';
+
+export default function Home() {
+
+  return (
+    <div>
+      {/* 이동 중일 때 보여줄 메시지나 로딩 스피너 등 */}
+      <div>desk management</div>
+    </div>
+  );
+}
